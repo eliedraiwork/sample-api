7016650e5d4b126df1a8dc1d49f0dfe6620b97df
elied-balink <elied@balink.net>
2022-02-01 23:55:50 +0200

    deploy error

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
     @Get()
     getHello(): string {
         return this.appService.getHello();
+    }
+
+    @Get('error')
+    getError(@Query('error_code') errorCode: string): string {
+        throw new HttpException('Error in the API', parseInt(errorCode, 10));
     }
 }
