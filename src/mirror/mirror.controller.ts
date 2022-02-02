13aef82bd5e1bacdfad0a498f640bfe6ae60677a
elied-balink <elied@balink.net>
2022-02-02 18:28:39 +0200

    add methods

--- src/mirror/mirror.controller.ts
+++ src/mirror/mirror.controller.ts
@@ -1,9 +1,27 @@
-import { Body, Controller, Get, Query, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query, Req } from '@nestjs/common';
 
 @Controller('mirror')
 export class MirrorController {
     @Get()
     mirrorRequest(@Body() body: any, @Query() query: any, @Req() req: any) {
+        return {
+            body,
+            query,
+            headers: req.headers,
+        };
+    }
+
+    @Post()
+    postMirrorRequest(@Body() body: any, @Query() query: any, @Req() req: any) {
+        return {
+            body,
+            query,
+            headers: req.headers,
+        };
+    }
+
+    @Delete()
+    deleteMirrorRequest(@Body() body: any, @Query() query: any, @Req() req: any) {
         return {
             body,
             query,
